ebebbdac796ac675aa433f8746a0f5432fc61327
Cas Ebbers <cjaebbers@gmail.com>
2012-05-28 19:15:53 +0200

    Config example & don't cause newline problems

--- bootstrap.php
+++ bootstrap.php
@@ -9,4 +9,5 @@ mysql_connect(MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD);
 mysql_select_db(MYSQL_DATABASE);
 
 $api = new EntreeAPI(BACKEND);
-?>
+
+// EOF
---
+++ config.example.php
@@ -0,0 +1,12 @@
+<?php
+
+/** Backend locatie **/
+define('BACKEND', 'http://localhost:3000');
+
+/** MySQL meuk **/
+define('MYSQL_HOST', 'localhost');
+define('MYSQL_USER', 'root');
+define('MYSQL_PASSWORD', '');
+define('MYSQL_DATABASE', 'terminal');
+
+// EOF
