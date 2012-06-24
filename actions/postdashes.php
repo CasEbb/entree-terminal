1d529a8bc6888741721c509b3fbc14de6b403448
Cas Ebbers <cjaebbers@gmail.com>
2012-06-24 17:49:12 +0200

    Stop verbose logging

--- actions/postdashes.php
+++ actions/postdashes.php
@@ -8,12 +8,8 @@ if($_COOKIE['date'] != date('d-m-Y') && date('G') >= 12) {
 } else {
   $dashes = unserialize($_COOKIE['dashes']);
 }
-
-$fp = fopen('log.txt', 'a');
-fputs($fp, "| ".date('d/m')." | ".date('H:i')."   | =============================\r\n");
  
 foreach($_POST['users'] as $num => $user_id) {
-  fputs($fp, "| " . date('d-m-Y H:i') . " | streepje[".$user_id.", ".$_POST['product_id']."]\r\n");
   if($dashes[$user_id]) { $dashes[$user_id] += 1; } else { $dashes[$user_id] = 1; }
   $api->post(array('dash[user_id]' => $user_id, 'dash[product_id]' => $_POST['product_id']), 'dashes');
 }
